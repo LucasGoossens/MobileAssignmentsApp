cbff713687b21a4966526e3cd855f91b25a212ad
LGZUYD <2305329goossens@zuyd.nl>
2024-12-05 16:44:25 +0100

    Profile

--- Profile.xaml.cs
+++ Profile.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Controls.Shapes;
 namespace InleverenWeek4MobileDev;
 
 public partial class Profile : ContentPage
@@ -5,5 +6,57 @@ public partial class Profile : ContentPage
 	public Profile()
 	{
 		InitializeComponent();
-	}
+
+        for (int i = 0; i < 4; i++)
+        {
+            var image = new Image
+            {
+                Source = "trending_clownfish",
+                Aspect = Aspect.AspectFill
+            };
+
+            var border = new Border
+            {
+                Stroke = Colors.CornflowerBlue,
+                StrokeThickness = 2,
+                BackgroundColor = Colors.LightGray,
+                StrokeShape = new RoundRectangle
+                {
+                    CornerRadius = new CornerRadius(5, 5, 5, 5)
+                },
+                WidthRequest = 150,
+                HeightRequest = 250,
+                Margin = new Thickness(5, 3, 3, 6),
+                Content = image
+            };
+
+            ProfileCreatedChallenges.Children.Add(border);
+        }
+
+
+        for (int i = 0; i < 4; i++)
+        {
+            var image = new Image
+            {
+                Source = "trending_clownfish",
+                Aspect = Aspect.AspectFill
+            };
+
+            var border = new Border
+            {
+                Stroke = Colors.CornflowerBlue,
+                StrokeThickness = 2,
+                BackgroundColor = Colors.LightGray,
+                StrokeShape = new RoundRectangle
+                {
+                    CornerRadius = new CornerRadius(5, 5, 5, 5)
+                },
+                WidthRequest = 150,
+                HeightRequest = 250,
+                Margin = new Thickness(5, 3, 3, 6),
+                Content = image
+            };
+            ProfileRecentSubmissions.Children.Add(border);
+        }
+    }
 }
